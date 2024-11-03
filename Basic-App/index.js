95976b8339345d1ffdefce1b5581473fc7b61c27
SumitRodrigues <sumitrod11@gmail.com>
2024-11-02 19:36:49 -0700

    created simple file to put on docker

--- Basic-App/index.js
+++ Basic-App/index.js
@@ -7,6 +7,24 @@ app.get("/", (req, res) => {
             id: 1,
             name: "Sumit Rodrigues",
             age: 24,
+        },
+        {
+            id: 2,
+            name: "Sam Sulek",
+            age: 29,
+        },
+        {
+            id: 3,
+            name: "Astel Bazinski",
+            age: 18,
+        },
+        {
+            id: 4,
+            name: "Blair Witch",
+            age: 21,
         }
     ])
+});
+app.listen(5500, ()=>{
+    console.log("app is running on 5500 port")
 })
