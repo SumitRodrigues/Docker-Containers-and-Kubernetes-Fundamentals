af4978b6cfd75f2cbfb470e2126bddac82d5d014
SumitRodrigues <sumitrod11@gmail.com>
2024-11-04 07:16:11 -0800

    volumes in docker

--- Basic-App/index.js
+++ Basic-App/index.js
@@ -5,17 +5,17 @@ app.get("/", (req, res) => {
     res.json([
         {
             id: 1,
-            name: "Sumit Rodrigues",
+            name: "Sumit Rodrigues SRODZ",
             age: 24,
         },
         {
             id: 2,
-            name: "Sam Sulek",
+            name: "Sam Sulek 200lbs",
             age: 29,
         },
         {
             id: 3,
-            name: "Astel Bazinski",
+            name: "Astel Bazinski babu",
             age: 18,
         },
         {
